04b7e64c38c1a2a00978931a50df163740c7026a
mahmouddabour <mahmouddabour@gmail.com>
2021-08-01 16:47:04 +0200

    GCPterraformtaskdone

--- 0Provider.tf
+++ 0Provider.tf
@@ -15,3 +15,15 @@ terraform {
 provider "random" {
   # Configuration options
 }
+
+resource "google_project_service" "project" {
+  project = "dabournti"
+  service = "compute.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "google_compute_firewall" "http-server" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["80","8080"]
   }
 
   // Allow traffic from everywhere to instances with an http-server tag
@@ -108,10 +108,93 @@ resource "google_compute_router_nat" "nat" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-# resource "google_service_account" "default" {
-#   account_id   = "hiii"
-#   display_name = "Service Account"
-# }
+resource "google_service_account" "default" {
+  account_id   = "dabournti"
+  display_name = "Service Account"
+}
+
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "dabourntiimagestorecom"
+  location      = "europe-west2"
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "europe-west2"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "dabournti2"
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/compute.instances.admin"
+
+    members = []
+  }
+}
+
+resource "google_service_account" "sa" {
+  account_id   = "dabournti3"
+  display_name = "A service account that only Jane can interact with"
+}
+
+resource "google_service_account_iam_policy" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  policy_data        = data.google_iam_policy.admin.policy_data
+}
+
+
+
+
 
 output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
